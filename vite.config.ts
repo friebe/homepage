556412d3935abd5b60b4a20499b480c6eeddaa11
Jan Friebe <friebe@odas.biz>
2025-04-18 21:24:01 +0200

    remove animation from background

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: '/index.html',
+        main: './index.html',
       },
       output: {
         manualChunks: {
